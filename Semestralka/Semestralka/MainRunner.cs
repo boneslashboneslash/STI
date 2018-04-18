e485b3ad9db7fb2f5f9457dddc6a6b2ec824c94b
Martin_Pecina <martin.pecina@tul.cz>
2018-04-18 19:26:46 +0200

    #commit added auto save textboxes for url and storage (new class SettingHandler), new class Runner for handle periodic checking repository

--- Semestralka/Semestralka/MainRunner.cs
+++ Semestralka/Semestralka/MainRunner.cs
@@ -13,45 +13,45 @@ namespace RepositoryModel
     {
         private static RepositoryGetter getter;
         private static readonly List<string> fileExt = new List<string>();
-        
-        public static void Main(string[] args)
-        {
-            // Getting user name and repository name from github url
-            var userNameRepositoryName = RepositoryGetter.parseUrl("https://github.com/martinspetlik/TGH");
- 
-            // Create new repository getter
-            getter = new RepositoryGetter(userNameRepositoryName.Item1, userNameRepositoryName.Item2);
-            // Authetification for 5000 request per hour
-            getter.Authentication().Wait();
 
-            // Check if repository exists, it is not part of contructor, if repository is changed make sure that this method is called
-            var repositoryExists = getter.CheckRepository().Result;
+        //public static void Main(string[] args)
+        //{
+        //    // Getting user name and repository name from github url
+        //    var userNameRepositoryName = RepositoryGetter.parseUrl("https://github.com/martinspetlik/TGH");
 
-            if (!repositoryExists) return;
+        //    // Create new repository getter
+        //    getter = new RepositoryGetter(userNameRepositoryName.Item1, userNameRepositoryName.Item2);
+        //    // Authetification for 5000 request per hour
+        //    getter.Authentication().Wait();
 
-            // Network checker
-            //Console.WriteLine("NETWORK " + System.Net.NetworkInformation.NetworkInterface.GetIsNetworkAvailable());
+        //    // Check if repository exists, it is not part of contructor, if repository is changed make sure that this method is called
+        //    var repositoryExists = getter.CheckRepository().Result;
 
-            // Testing file extension
-            fileExt.Add("java");
-            getter.FileExtensions = fileExt;
+        //    if (!repositoryExists) return;
 
-            Console.WriteLine("Files extensions line number" + getter.FileExtensionsLinesNumber().Result["java"]);
+        //    // Network checker
+        //    //Console.WriteLine("NETWORK " + System.Net.NetworkInformation.NetworkInterface.GetIsNetworkAvailable());
 
-            var filesExtensions = getter.ChangedFiles(new DateTime(2016, 4, 19, 20, 22, 12)).Result;
-            
-            // Loop through filesExtensions
-            // Dictionary {fileName: List(commit1[number of changes, file content], commit2, ...)}
-            /*foreach(KeyValuePair<string, List<string[]>> entry in filesExtensions)
-            {
-                foreach (string[] commit in entry.Value)
-                {
-                    // commit is array -> []
-                    Console.WriteLine(string.Join(",", commit));
-                }
+        //    // Testing file extension
+        //    fileExt.Add("java");
+        //    getter.FileExtensions = fileExt;
+
+        //    Console.WriteLine("Files extensions line number" + getter.FileExtensionsLinesNumber().Result["java"]);
+
+        //    var filesExtensions = getter.ChangedFiles(new DateTime(2016, 4, 19, 20, 22, 12)).Result;
+
+        //    // Loop through filesExtensions
+        //    // Dictionary {fileName: List(commit1[number of changes, file content], commit2, ...)}
+        //    /*foreach(KeyValuePair<string, List<string[]>> entry in filesExtensions)
+        //    {
+        //        foreach (string[] commit in entry.Value)
+        //        {
+        //            // commit is array -> []
+        //            Console.WriteLine(string.Join(",", commit));
+        //        }
                 
-            }*/
+        //    }*/
 
-        }
+        //}
     }
 }
